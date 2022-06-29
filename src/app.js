efe01e46284a283547b2521ae73eef4752bd6189
Elena <33602691+eporubin@users.noreply.github.com>
2022-06-29 18:10:19 +0200

    added the header to the project

---
+++ src/app.js
@@ -0,0 +1,18 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "50%";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
